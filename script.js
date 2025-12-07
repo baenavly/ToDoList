4e370f5e174bfba84d9d3fc90855162f13fe166b
Nayeon <baenavly@yonsei.ac.kr>
2025-12-07 21:37:22 +0900

    [ADD] 유저 네임 등록, 투두 등록(로컬 미저장)

---
+++ script.js
@@ -0,0 +1,50 @@
+const greeting = document.getElementById("greeting");
+const nameInputArea = document.getElementById("nameInputArea");
+const nameInput = document.getElementById("nameInput");
+const saveNameBtn = document.getElementById("saveNameBtn");
+const todoArea = document.getElementById("todoArea")
+
+function showGreeting() {
+  const savedName = localStorage.getItem("name");
+  todoArea.style.display = "none";
+
+  if (savedName) {
+    greeting.textContent = `${savedName}님 안녕하세요!`;
+    nameInputArea.style.display = "none";
+    todoArea.style.display = "block";
+  }
+}
+
+showGreeting();
+
+saveNameBtn.addEventListener("click", () => {
+    const name = nameInput.value.trim();
+    if (name === "") return;
+
+    localStorage.setItem("name", name);
+    showGreeting();
+});
+
+const todoInput = document.getElementById("todoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+const list = document.getElementById("todoList");
+
+addTodoBtn.addEventListener("click", () => {
+    const text = todoInput.value.trim();
+    if (text === "") return;
+
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+
+    li.appendChild(checkbox);
+    li.appendChild(span);
+
+    list.appendChild(li);
+
+    todoInput.value = "";
+});
